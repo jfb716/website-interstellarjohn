5d72a8b38ccf57a5177be0529b82e9ea43eb2de2
John Black <john.f.black@icloud.com>
2016-08-20 20:34:28 -0400

    added jquery practice to profile image

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $(".profile").ready(function(){
+        $(".profile").fadeOut(1000);
+    });
+    $(".profile").ready(function(){
+        $(".profile").fadeIn(1000);
+    });
+});
+
